375360c1622e87817ebf89a04093757e50ea6a4a
Chenbo Yi <yichenbo@outlook.com>
2019-03-27 12:34:00 +0000

    Add test for the number 1

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  describe 'fizzbuzz' do
+    it 'returns "1" when passed 1' do
+      expect(fizzbuzz(1)).to eq '1'
+    end
+
 end
